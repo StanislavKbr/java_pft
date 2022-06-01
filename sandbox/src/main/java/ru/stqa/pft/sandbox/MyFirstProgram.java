6b726698aa03fd51a687ab99c7028b7ad0a501e8
unknown <84578871+StanislavKbr@users.noreply.github.com>
2022-06-01 21:08:53 +0300

    Переменные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,5 +4,9 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello,world");
+
+    int l = 8;//переменная
+    int s = l * l;//синооним для вычесленного значения
+    System.out.println("Площадь прямоугольника " + l + " = " + (l + l));//как итог упрощение написание
   }
 }
